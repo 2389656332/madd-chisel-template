866fb98b347babfc37943c76ca44fffb5849f8e6
2389656332 <132430125+2389656332@users.noreply.github.com>
2023-06-16 14:00:35 +0800

    Update StridePrefetcherIO.scala

--- src/main/scala/prefetcher/StridePrefetcherIO.scala
+++ src/main/scala/prefetcher/StridePrefetcherIO.scala
@@ -4,12 +4,13 @@ import chisel3._
 import chisel3.util._
 
 // TODO: update this module to implement stride prefetcher's IO.
-class StridePrefetcherIO(M: Int, N: Int) extends Bundle {
-  val a = Input(Vec(M * N, SInt(32.W)))
-  val b = Input(Vec(M * N, SInt(32.W)))
-
-  val out = Output(Vec(M * N, SInt(32.W)))
-
-  override def cloneType =
-    new StridePrefetcherIO(M, N).asInstanceOf[this.type]
-}
+class StridePrefetcherIO(addressWidth: Int, pcWidth: Int) extends Bundle {
+    
+    // 输入端口
+    val pc = Input(UInt(pcWidth.W))
+    val address = Input(UInt(addressWidth.W))
+    
+    // 输出端口
+    val prefetch_address = Output(UInt(addressWidth.W))
+    val prefetch_valid = Output(Bool())
+  }
